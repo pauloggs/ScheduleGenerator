635d69f7ae4295a9d7e097e0458fcf43cbb53359
pauloggs <pshodgkinson@googlemail.com>
2022-02-19 19:56:43 +0000

    Added basic unit tests for converter service.

--- ScheduleGenerator.Tests/ConverterServiceUnitTests.cs
+++ ScheduleGenerator.Tests/ConverterServiceUnitTests.cs
@@ -1,7 +1,50 @@
 ﻿namespace ScheduleGenerator.Tests
 {
+    using ScheduleGenerator.Services;
+    using ScheduleGenerator.Model;
+    using System.Collections.Generic;
+    using Xunit;
+    using Newtonsoft.Json;
+    using System;
+
     public class ConverterServiceUnitTests
     {
+        private readonly ConverterService sut;
 
+        public ConverterServiceUnitTests()
+        {
+            sut = new ConverterService();
+        }
+
+        [Fact]
+        public void GetRecipies_ReturnsListOfRecipesIfRawDataCorrectlyFormatted()
+        {
+            // Arrange
+            var validRawData = JsonConvert.SerializeObject(
+                new
+                {
+                    recipes = new List<Recipe>()
+                    {
+                        new Recipe("SomeRecipeName")
+                    }
+                }
+            );
+
+            // Act
+            var result = sut.GetRecipies(validRawData);
+
+            // Assert
+            Assert.Single(result);
+        }
+
+        [Fact]
+        public void GetRecipies_ThrowsExceptionIfRawDataIncorrectlyFormatted()
+        {
+            // Arrange
+            var invalidRawData = "SomeInvalidJson";
+
+            // Assert
+            Assert.Throws<Exception>(() => sut.GetRecipies(invalidRawData));
+        }
     }
 }
--- ScheduleGenerator/Services/ConverterService.cs
+++ ScheduleGenerator/Services/ConverterService.cs
@@ -9,15 +9,24 @@ namespace ScheduleGenerator.Services
         List<Recipe> GetRecipies(string rawData);
     }
     
+    // should, return a List<Recipe> if provided with a correctly formatted rawData
+    // should, throw an exception if there are any issues with deserialisation
     public class ConverterService : IConverterService
     {
         public List<Recipe> GetRecipies(string rawData)
         {
-            var o = JsonConvert.DeserializeObject<JObject>(rawData);
+            try
+            {
+                var o = JsonConvert.DeserializeObject<JObject>(rawData);
 
-            var recipes = o.Value<JArray>("recipes").ToObject<List<Recipe>>();
+                var recipes = o.Value<JArray>("recipes").ToObject<List<Recipe>>();
 
-            return recipes;
+                return recipes;
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Exception in GetRecipies: {e.Message}");
+            }            
         }
     }
 }
