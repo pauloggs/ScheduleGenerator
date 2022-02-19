80a4cebc1f031fe6920fe464ba3d5db956f41b7c
pauloggs <pshodgkinson@googlemail.com>
2022-02-19 11:17:29 +0000

    Add HttpClient to MapGet.

--- ScheduleGenerator/Program.cs
+++ ScheduleGenerator/Program.cs
@@ -2,15 +2,24 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHttpClient();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(opt =>
+    {
+        opt.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        opt.RoutePrefix = string.Empty;
+    });
 }
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/generate", async (HttpClient hc) => 
+{
+    await Task.Delay(1);
+    return "Hello world!";
+});
 
 app.Run();
